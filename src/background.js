7b43955d1ec5494e443e7a5cccbfb5e33377f682
rofe <me@rofe.com>
2019-04-17 22:50:28 +0200

    refactoring from PageAction to BrowserAction

--- src/background.js
+++ src/background.js
@@ -17,34 +17,42 @@ if (typeof HelixMarkdownPreview === 'undefined') {
   throw new Error('HelixMarkdownPreview is undefined');
 }
 
-// upon installation, define where the extension should be active
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: use rules from configuration
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {
-            hostSuffix: 'github.com',
-            pathContains: '/edit/',
-            pathSuffix: '.md',
-          },
-        }),
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {
-            hostEquals: 'raw.githubusercontent.com',
-            pathSuffix: '.md',
-          },
-        }),
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()],
-    }]);
+// check if the provided tab is allowed to use this extension
+// TODO: make configurable?
+function checkTab(tabId) {
+  chrome.tabs.get(tabId, (tab) => {
+    if (tab && tab.url) {
+      // enable extension if raw markdown
+      if (tab.url.match(/^https:\/\/raw\.githubusercontent\.com\/.*\.md$/)) {
+        chrome.browserAction.enable(tab.id);
+        return;
+      }
+      // enable extension if markdown editor
+      if (tab.url.match(/^https:\/\/.*github\.com\/.*\/edit\/.*\.md$/)) {
+        chrome.browserAction.enable(tab.id);
+        return;
+      }
+      // provide badge with hint if blob view
+      if (tab.url.match(/^https:\/\/.*github\.com\/.*\/blob\/.*\.md$/)) {
+        chrome.browserAction.setBadgeText({ text: '?', tabId });
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#c3003c', tabId });
+        chrome.browserAction.setTitle({ title: 'Start editing to enable markdown preview', tabId });
+      }
+    }
+    chrome.browserAction.disable(tabId);
   });
-  // console.log('Extension installed');
+}
+
+// listen for tab changes on check eligibility
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  checkTab(tabId);
+});
+chrome.tabs.onUpdated.addListener((tabId) => {
+  checkTab(tabId);
 });
 
-// when the page action is clicked, toggle the markdown preview
-chrome.pageAction.onClicked.addListener((tab) => {
+// when the browser action is clicked, toggle the markdown preview
+chrome.browserAction.onClicked.addListener((tab) => {
   HelixMarkdownPreview.getReceiver((receiver) => {
     // load scripts inside the content tab
     chrome.tabs.executeScript(tab.id, {
